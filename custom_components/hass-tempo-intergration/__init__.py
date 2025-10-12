1234963984dae82749f3e5de5ec60ff634742673
Thomas Cousin <thomas.cousin@me.com>
2025-10-12 23:44:27 +0100

    Other updates to Init

--- custom_components/hass-tempo-intergration/__init__.py
+++ custom_components/hass-tempo-intergration/__init__.py
@@ -1,8 +1,16 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN, PLATFORMS
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, entry: ConfigEntry):
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     # Return boolean to indicate that initialization was successful.
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
